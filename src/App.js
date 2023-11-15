45291dbd3d9bd04a9b9fafd677dc3437f42b32ef
daehyeong2 <baconbacon1231@gmail.com>
2023-11-15 16:55:19 +0900

    5.1 ~ 6.2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { useState, useEffect } from "react";
+
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
+  console.log("render (all)");
+  useEffect(() => {
+    console.log("render (first)");
+  }, []);
+  useEffect(() => {
+    if (keyword && keyword.length >= 3) {
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]);
   return (
     <div>
-      <h1>Hello World!</h1>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here"
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me!</button>
     </div>
   );
 }
