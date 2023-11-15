7e8c3bd90b137c003f00fd908a4ba6d507701fed
daehyeong2 <baconbacon1231@gmail.com>
2023-11-15 17:35:43 +0900

    6.4

--- src/App.js
+++ src/App.js
@@ -1,34 +1,20 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
+
+function Hello() {
+  useEffect(() => {
+    console.log("created.");
+    return () => console.log("destroyed.");
+  }, []);
+  return <h1>Hello</h1>;
+}
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setCounter((prev) => prev + 1);
-  const onChange = (event) => {
-    setKeyword(event.target.value);
-  };
-  console.log("render (all)");
-  useEffect(() => {
-    console.log("render (first)");
-  }, []);
-  useEffect(() => {
-    if (keyword && keyword.length >= 3) {
-      console.log("SEARCH FOR", keyword);
-    }
-  }, [keyword]);
-  useEffect(() => {
-    console.log("render(keyword || counter)");
-  }, [keyword, counter]);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here"
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me!</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
